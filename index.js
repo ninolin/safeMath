84b53dc937162f778db66e1d009ae8328b7e262c
Nino Lin <nino.lin@isuncloud.com>
2018-03-15 17:32:38 +0800

    update mule

--- index.js
+++ index.js
@@ -38,10 +38,9 @@ const safeMath = class {
     static safeMule(x,y) {
         let x_decimals = (x.toString().split(".")[1] != undefined) ? x.toString().split(".")[1].length : 0;
         let y_decimals = (y.toString().split(".")[1] != undefined) ? y.toString().split(".")[1].length : 0;
-        let max_decimals = (x_decimals >= y_decimals) ? x_decimals : y_decimals;
         (x_decimals!=0) && (x = x.toString().split(".")[0]+x.toString().split(".")[1]);
         (y_decimals!=0) && (y = y.toString().split(".")[0]+y.toString().split(".")[1]);
-        return (x*y)/10**max_decimals;
+        return (x*y)/10**(x_decimals+y_decimals);
     }
     static safeDiv(x,y) {
         let x_decimals = (x.toString().split(".")[1] != undefined) ? x.toString().split(".")[1].length : 0;
